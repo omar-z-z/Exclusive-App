a2454f1ff77b379806c61ff9fc7995c4471c6086
Omar Zuhri <omar.z.zuhri@gmail.com>
2025-10-04 17:14:03 +0300

    Resposive 2

--- src/components/benefits/BenefitItem.jsx
+++ src/components/benefits/BenefitItem.jsx
@@ -1,10 +1,12 @@
 const BenefitItem = ({ icon, title, subtitle }) => (
-  <div className="flex flex-col items-center gap-3 text-center">
-    <div className="grid h-28 w-28 place-items-center rounded-full bg-gray-300">
-      <div className="grid h-18 w-18 place-items-center rounded-full bg-black text-white">{icon}</div>
+  <div className="flex flex-col items-center gap-2 text-center">
+    <div className="grid place-items-center rounded-full bg-gray-300 w-16 h-16 sm:w-28 sm:h-28">
+      <div className="grid place-items-center rounded-full bg-black text-white w-10 h-10 sm:w-16 sm:h-16">
+        {icon}
+      </div>
     </div>
-    <div className="text-xl font-bold text-black">{title}</div>
-    <div className="text-base font-semibold text-gray-800">{subtitle}</div>
+    <div className="text-xs sm:text-sm font-bold text-black">{title}</div>
+    <div className="text-[10px] sm:text-sm font-semibold text-gray-800">{subtitle}</div>
   </div>
 );
 
--- src/components/benefits/Benefites.jsx
+++ src/components/benefits/Benefites.jsx
@@ -4,7 +4,7 @@ import { TruckIcon, HeadsetIcon, ShieldIcon } from "../basicComponents/icons";
 
 const Benefits = () => (
   <Section className="pt-4">
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+    <div className="grid grid-cols-3 gap-6">
       <BenefitItem title="FREE AND FAST DELIVERY" subtitle="Free delivery for all orders over $140" icon={<TruckIcon />} />
       <BenefitItem title="24/7 CUSTOMER SERVICE" subtitle="Friendly 24/7 customer support" icon={<HeadsetIcon />} />
       <BenefitItem title="MONEY BACK GUARANTEE" subtitle="We return money within 30 days" icon={<ShieldIcon />} />
